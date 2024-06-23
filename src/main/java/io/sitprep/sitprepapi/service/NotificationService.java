11278a2536ef31f4d557056d30efd943aea4ae0b
Dione Pierre-Louis <francisd.plouis@gmail.com>
2024-06-23 02:36:44 -0600

    sending to users at once

--- src/main/java/io/sitprep/sitprepapi/service/NotificationService.java
+++ src/main/java/io/sitprep/sitprepapi/service/NotificationService.java
@@ -3,10 +3,13 @@ package io.sitprep.sitprepapi.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -15,26 +18,25 @@ public class NotificationService {
     private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
     public void sendNotification(String title, String body, Set<String> tokens) {
-        for (String token : tokens) {
-            logger.info("Sending notification to token: {}", token);
+        List<String> registrationTokens = List.copyOf(tokens);
 
-            // Log the payload being sent
-            logger.info("Notification Payload - Title: {}, Body: {}, Token: {}", title, body, token);
+        MulticastMessage message = MulticastMessage.builder()
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .setImage("/images/icon-120.png")
+                        .build())
+                .putData("badge", "/images/icon-512.png")
+                .putData("requireInteraction", "true")
+                .putData("sound", "default")
+                .addAllTokens(registrationTokens)
+                .build();
 
-            Message notificationMessage = Message.builder()
-                    .setToken(token)
-                    .putData("title", title)
-                    .putData("body", body)
-                    .putData("icon", "/images/icon-120.png")
-                    .putData("badge", "/images/icon-512.png")
-                    .build();
-
-            try {
-                String response = FirebaseMessaging.getInstance().send(notificationMessage);
-                logger.info("Successfully sent message: {}", response);
-            } catch (FirebaseMessagingException e) {
-                logger.error("Error sending message: {}", e.getMessage(), e);
-            }
+        try {
+            FirebaseMessaging.getInstance().sendMulticast(message);
+            logger.info("Successfully sent message to multiple devices.");
+        } catch (FirebaseMessagingException e) {
+            logger.error("Error sending message: {}", e.getMessage(), e);
         }
     }
 }
