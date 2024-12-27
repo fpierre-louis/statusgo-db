a0e20baed1d5a694ac94e9fc1e9bc77b5ef19c3f
Dione Pierre-Louis <francisd.plouis@gmail.com>
2024-12-27 06:19:01 -0700

    doing hard coding

--- src/main/java/io/sitprep/sitprepapi/service/NotificationService.java
+++ src/main/java/io/sitprep/sitprepapi/service/NotificationService.java
@@ -14,15 +14,23 @@ public class NotificationService {
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
-    public void sendNotification(String title, String body, String iconUrl, String from, Set<String> tokens, String type, String groupId) {
+    public void sendNotification(String title, String body, String iconUrl, String imageUrl, String sound, String from, Set<String> tokens, String type, String groupId) {
         for (String token : tokens) {
             logger.info("Sending notification to token: {}", token);
+
+            // Use provided or default values
+            String iconUrlToUse = iconUrl != null ? iconUrl : "https://firebasestorage.googleapis.com/v0/b/sitprep-new.appspot.com/o/icons%2Fdefault-icon.png?alt=media";
+            String imageUrlToUse = imageUrl != null ? imageUrl : "https://firebasestorage.googleapis.com/v0/b/sitprep-new.appspot.com/o/posts%2Fpost-image.png?alt=media";
+            String soundToUse = sound != null ? sound : "default"; // Use "default" or a custom sound file
 
             Message notificationMessage = Message.builder()
                     .setToken(token)
                     .putData("title", title)
                     .putData("body", body)
-                    .putData("icon", iconUrl)
+                    .putData("icon", iconUrlToUse)  // Small icon for the notification
+                    .putData("image", imageUrlToUse)  // Large image for the notification body
+                    .putData("sound", soundToUse)  // Sound to play when notification is received
+                    .putData("badge", "1") // Badge count, typically an integer
                     .putData("type", type)
                     .putData("groupId", groupId)
                     .build();
@@ -35,4 +43,5 @@ public class NotificationService {
             }
         }
     }
+
 }
