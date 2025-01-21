1b0e9b37782e1f7d80d816e70aac89efc032b031
Dione Pierre-Louis <francisd.plouis@gmail.com>
2025-01-21 00:08:16 -0700

    update code

--- src/main/java/io/sitprep/sitprepapi/resource/MealPlanResource.java
+++ src/main/java/io/sitprep/sitprepapi/resource/MealPlanResource.java
@@ -5,6 +5,8 @@ import io.sitprep.sitprepapi.service.MealPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
 
 import java.util.List;
 
